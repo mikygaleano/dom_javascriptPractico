63eafbf3220d3d9dc1fca13a3e2eccb5fae16a4c
mikepipagaleano <mikebostero.bernal@gmail.com>
2022-08-24 16:59:45 -0300

    portafolio

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+import scrollTopButton from "./Dom/boton-scroll.js";
 import hamburgerMenu from "./Dom/menu-hamburguesa.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
-    hamburgerMenu(".panel-btn", ".panel", ".menu a")
+    hamburgerMenu(".panel-btn", ".panel", ".menu a");
+
+    scrollTopButton(".scroll-top-bt");
 })
