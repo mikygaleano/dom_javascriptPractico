3a4a9d2bcd8c5d6bd885792b6f0750a6c247e131
mikepipagaleano <mikebostero.bernal@gmail.com>
2022-09-01 01:08:27 -0300

    portafolio

--- Dom/typed-home.js
+++ Dom/typed-home.js
@@ -1,13 +1,7 @@
-import Typed from 'typed.js';
-export default  function typedHome(html) {
-    const maquina = {
-        strings: ["<i>Primeira</i> frase com Typed.js.", "Olá MundoJS!"],
-        typeSpeed: 40,
-    }
-        //let typed = document.querySelector(heading);
-        Typed = new Typed ('.element');
-        
-        texto = document.querySelector(".heading");
 
-        html = texto.outerHTML(maquina);
-}
+
+     
+    
+
+
+
--- index.js
+++ index.js
@@ -14,3 +14,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
 
 })
 
+
+
+
+
